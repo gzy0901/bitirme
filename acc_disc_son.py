4576aeb697aa6885ac32782a0b6a6016fd5b5a03
gzy0901 <82293039+gzy0901@users.noreply.github.com>
2022-10-22 14:10:56 +0300

    Update acc_disc_son.py

--- acc_disc_son.py
+++ acc_disc_son.py
@@ -4,6 +4,7 @@
 
 from numpy import exp
 from mpmath import *
+
 mp.dps = 300
 
 G = 6.6743e-8 # cm^3/g.s
@@ -22,40 +23,61 @@ pc = 3.086e+18 #parsec in cm
 c = 3e+10 #cm/s
 k = 1.3807e-16 #cm^2.g/s^2.K
 x = R_out/R_in
+T_s = (3*G*M*M_dot)/(8*pi*R_in*sigma)
+#T_x = T_s*x**(-3/4)*(1-x(-1/2))**(1/4) this gives an error because of the complexity of the expression
+T_x = T_s*pow(x, (-3/4))*pow((1-pow(x, (-1/2))), (1/4))
+lower_limit = R_in
+upper_limit = R_out
+sub_interval = 50
 v = 1e16 #frequency
 
-while (v <1e20):
-
-    T_s = (3*G*M*M_dot)/(8*pi*R_in*sigma)
-    #T_x = T_s*x**(-3/4)*(1-x(-1/2))**(1/4) this gives an error because of the complexity of the expression
-    T_x = T_s*pow(x, (-3/4))*pow((1-pow(x, (-1/2))), (1/4))
-
-    # Define function to integrate
-    def f(x):
-        return(mpf((4*pi*h*cosi*v**3*R_in**2)/(c**2*D**2)*(x/exp(h*v/k*T_x)-1)))
-
-    # Implementing Simpson's 1/3 
-    def simpson13(a,b,n):
-        if n<1 or a>b:
-            print ("Hatali veri!")
-        elif n%2 == 1:
-            print ("n cift degil!")
-        else:
-            h = (b-a)/n
-            s = f(a) + f(b)
-            for i in range (1,n):
-                katsayi = 2*(i%2+1)
-                x = a+i*h
-                s = s+katsayi*f(x)
-        return h*s/3.0
-
+def main():
+    global v
+    
+    #empty list
+    frequency = []
+    flux = []
+    
+    while (v < 1.5*1e16):
+        
+        v*=1.5
    
-
-    lower_limit = R_in
-    upper_limit = R_out
-    sub_interval = 50
+        frequency.append(v)
+        
+        flux.append(f(x))
 
     # Call trapezoidal() method and get result
     result = simpson13(lower_limit, upper_limit, sub_interval)
-    v+=1.5
-print(f"Integration result by Simpson's 1/3 method is: {result}" )
+
+    print(f"Integration result by Simpson's 1/3 method is: {result}" )
+    
+    print(frequency)
+    print(flux)
+    
+    # for i in frequency:
+    #     print (i)
+    # for i in flux:
+    #     print (i)
+
+        
+# Define function to integrate
+def f(x):
+    return(mpf((4*pi*h*cosi*v**3*R_in**2)/(c**2*D**2)*(x/exp(h*v/k*T_x)-1)))
+
+# Implementing Simpson's 1/3 
+def simpson13(a,b,n):
+    if n<1 or a>b:
+        print ("Hatali veri!")
+    elif n%2 == 1:
+        print ("n cift degil!")
+    else:
+        h = (b-a)/n
+        s = f(a) + f(b)
+        for i in range (1,n):
+            katsayi = 2*(i%2+1)
+            x = a+i*h
+            s = s+katsayi*f(x)
+    return h*s/3.0
+
+    
+main()
